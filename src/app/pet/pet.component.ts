26e80b26b34f274cc2043ba99b83c541916fb09f
thienhoang563 <thienhoang563@gmail.com>
2019-03-15 11:48:04 +0700

    Pet information

---
+++ src/app/pet/pet.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet',
+  templateUrl: './pet.component.html',
+  styleUrls: ['./pet.component.css']
+})
+export class PetComponent implements OnInit {
+  petName = 'puppie';
+  petImage = 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12225919/Pembroke-Welsh-Corgi-On-White-01.jpg';
+  constructor() { }
+  updateName(name) {
+    this.petName = name;
+  }
+
+  updateImage(image) {
+    this.petImage = image;
+  }
+
+  ngOnInit() {
+  }
+
+}
